f971e2cc9b6bdb13a271e20736efffa4d173b215
RaoufEd <raouf.edabbar@gmail.com>
2017-04-16 14:28:40 +0100

    fixed add lieu

--- GestionRandonnee/src/gestionrandonnee/services/LieuService.java
+++ GestionRandonnee/src/gestionrandonnee/services/LieuService.java
@@ -1,3 +1,4 @@
+<<<<<<< refs/remotes/origin/heni
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,3 +13,52 @@ package gestionrandonnee.services;
 public class LieuService {
     
 }
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionrandonnee.services;
+
+import gestionrandonnees.entities.Lieu;
+import gestionrandonnees.utils.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Raouf
+ */
+public class LieuService {
+
+    DataSource ds;
+    PreparedStatement ste;
+
+    public LieuService() {
+
+        ds = DataSource.getInstance(); // au lieu de creer une instance on fait appel a 
+        //la methode de creation du classe DataSource
+
+    }
+
+    public void ajouter(Lieu l) {
+        String req = "INSERT INTO lieu VALUES(null,?,?,?)";
+        try {
+            ste = ds.getConnection()
+                    .prepareStatement(req);
+
+           
+            ste.setDouble(1, l.getLongitude());
+            ste.setDouble(2, l.getLatitude());
+            ste.setString(3, l.getLieuText());
+            ste.executeUpdate();
+            /*executeUpdate methode d'ajout modification ou supression*/
+        } catch (SQLException ex) {
+            Logger.getLogger(LieuService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+>>>>>>> fixed add lieu
