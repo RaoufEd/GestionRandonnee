a8bd64d2cb4562edd20128ed5808e4cd10c5f310
heni <carelessly.living@gmail.com>
2017-04-20 22:45:51 +0200

    Media Source

--- GestionRandonnee/src/gestionrandonnee/services/MediaService.java
+++ GestionRandonnee/src/gestionrandonnee/services/MediaService.java
@@ -5,10 +5,39 @@
  */
 package gestionrandonnee.services;
 
+import gestionrandonnees.entities.Media;
+import gestionrandonnees.utils.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author heni
  */
 public class MediaService {
+    
+    DataSource ds;
+    PreparedStatement ste;
+    
+    public MediaService() {
+        ds = DataSource.getInstance();
+    }
+    
+    public void ajouter(Media m) {
+        String query = "INSERT INTO media VALUES (null, ?, ?, ?, ?)";
+        try {
+            ste.getConnection().prepareStatement(query);
+            ste.setInt(1, m.getRandonnee().getIdRandonnee());
+            ste.setInt(2, m.getRandonneur().getIdRandonneur());
+            ste.setString(3, m.getNom());
+            ste.setString(4, m.getExtension());
+            ste.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(MediaService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     
 }
